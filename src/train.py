7d14b1c2e4aca6cfd5fcc219e1a9375ec607397f
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2026-05-22 22:39:15 -0400

    Refactor training function to include early stopping and metrics computation; update output directory handling and benchmark naming

--- src/train.py
+++ src/train.py
@@ -8,14 +8,18 @@ from adapters import (
 from transformers import (
     AutoTokenizer,
     DataCollatorWithPadding,
+    EarlyStoppingCallback,
     PreTrainedModel,
     PreTrainedTokenizerBase,
 )
 from transformers.training_args import TrainingArguments
 
 from src.data import load_data_dil
+from src.metrics import MulticlassClassificationMetrics
 
 AdaptersModel = ModelWithFlexibleHeadsAdaptersMixin | PreTrainedModel
+
+compute_metrics = MulticlassClassificationMetrics()
 
 
 def train(
@@ -23,11 +27,28 @@ def train(
     tokenizer: PreTrainedTokenizerBase,
     train_dataset,
     valid_dataset,
+    output_dir: Path | str | None = None,
+    num_train_epochs: int = 20,
 ):
+    if output_dir:
+        output_dir = Path(output_dir)
+    else:
+        output_dir = None
     training_args = TrainingArguments(
+        output_dir=output_dir,
         learning_rate=1e-4,
-        num_train_epochs=6,
+        num_train_epochs=num_train_epochs,
         report_to="none",
+        eval_strategy="epoch",
+        save_strategy="best",
+        load_best_model_at_end=True,
+        push_to_hub=False,
+        metric_for_best_model="f1_macro",
+        greater_is_better=True,
+    )
+    early_stopping_callback = EarlyStoppingCallback(
+        early_stopping_patience=3,
+        early_stopping_threshold=0.001,
     )
     data_collator = DataCollatorWithPadding(
         tokenizer=tokenizer,
@@ -41,6 +62,10 @@ def train(
         eval_dataset=valid_dataset,
         tokenizer=tokenizer,
         data_collator=data_collator,
+        callbacks=[
+            early_stopping_callback,
+        ],
+        compute_metrics=compute_metrics,
     )
     trainer.train()
 
@@ -54,7 +79,7 @@ def print_active_parameters(model: AdaptersModel):
 
 def main(
     model_name: str = "roberta-base",
-    base_name: str = "stance",
+    benchmark: str = "stance",
     output_dir: str | Path = "output",
 ):
     output_dir: Path = Path(output_dir)
@@ -67,10 +92,10 @@ def main(
 
     model: AdaptersModel = AutoAdapterModel.from_pretrained(model_name)
 
-    task_head = f"{base_name}-head"
+    task_head = f"{benchmark}-head"
     model.add_classification_head(task_head, num_labels=num_labels)
 
-    lora_adapter_name = f"{base_name}-lora"
+    lora_adapter_name = f"{benchmark}-lora"
     model.add_adapter(lora_adapter_name, "lora")
     model.train_adapter(lora_adapter_name)
 
@@ -78,6 +103,9 @@ def main(
     model.active_head = task_head
 
     for task in tasks:
+        # same model is trained sequentially on all tasks,
+        #     Domain-IL - the head is shared across all tasks
+        task_output_dir = output_dir / f"adapter-{benchmark}-{task.name}"
         train_dataset = task["train"]
         eval_dataset = task["valid"]
         train(
@@ -85,10 +113,10 @@ def main(
             tokenizer=tokenizer,
             train_dataset=train_dataset,
             valid_dataset=eval_dataset,
+            output_dir=task_output_dir / "checkpoints",
         )
-        adapter_save_path = output_dir / f"adapter-{base_name}-{task.name}"
-        model.save_adapter(adapter_save_path, lora_adapter_name, with_head=False)
-        model.save_head(str(adapter_save_path), task_head)
+        model.save_adapter(task_output_dir, lora_adapter_name, with_head=False)
+        model.save_head(str(task_output_dir), task_head)
 
 
 if __name__ == "__main__":
